4a3993e08d1cfe2f92ee19b884ee3a5bf79dc773
ogulicat <gulicat.ozan@gmail.com>
2018-03-06 21:15:03 +0300

    Initial Commit

---
+++ iller.sql
@@ -0,0 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1:3306
+-- Üretim Zamanı: 06 Mar 2018, 18:10:54
+-- Sunucu sürümü: 5.7.21-log
+-- PHP Sürümü: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `ders`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `iller`
+--
+
+DROP TABLE IF EXISTS `iller`;
+CREATE TABLE IF NOT EXISTS `iller` (
+  `il_id` int(11) NOT NULL AUTO_INCREMENT,
+  `baslik` varchar(50) NOT NULL,
+  PRIMARY KEY (`il_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `iller`
+--
+
+INSERT INTO `iller` (`il_id`, `baslik`) VALUES
+(1, 'Adana'),
+(2, 'Adıyaman'),
+(3, 'Afyonkarahisar'),
+(4, 'Ağrı'),
+(5, 'Amasya'),
+(6, 'Ankara'),
+(7, 'Antalya'),
+(8, 'Artvin'),
+(9, 'Aydın'),
+(10, 'Balıkesir'),
+(11, 'Bilecik'),
+(12, 'Bingöl'),
+(13, 'Bitlis'),
+(14, 'Bolu'),
+(15, 'Burdur'),
+(16, 'Bursa'),
+(17, 'Çanakkale'),
+(18, 'Çankırı'),
+(19, 'Çorum'),
+(20, 'Denizli'),
+(21, 'Diyarbakır'),
+(22, 'Edirne'),
+(23, 'Elazığ'),
+(24, 'Erzincan'),
+(25, 'Erzurum'),
+(26, 'Eskişehir'),
+(27, 'Gaziantep'),
+(28, 'Giresun'),
+(29, 'Gümüşhane'),
+(30, 'Hakkari'),
+(31, 'Hatay'),
+(32, 'Isparta'),
+(33, 'Mersin(İçel)'),
+(34, 'İstanbul'),
+(35, 'İzmir'),
+(36, 'Kars'),
+(37, 'Kastamonu'),
+(38, 'Kayseri'),
+(39, 'Kırklareli'),
+(40, 'Kırşehir'),
+(41, 'Kocaeli'),
+(42, 'Konya'),
+(43, 'Kütahya'),
+(44, 'Malatya'),
+(45, 'Manisa'),
+(46, 'Kahramanmaraş'),
+(47, 'Mardin'),
+(48, 'Muğla'),
+(49, 'Muş'),
+(50, 'Nevşehir'),
+(51, 'Niğde'),
+(52, 'Ordu'),
+(53, 'Rize'),
+(54, 'Sakarya'),
+(55, 'Samsun'),
+(56, 'Siirt'),
+(57, 'Sinop'),
+(58, 'Sivas'),
+(59, 'Tekirdağ'),
+(60, 'Tokat'),
+(61, 'Trabzon'),
+(62, 'Tunceli'),
+(63, 'Şanlıurfa'),
+(64, 'Uşak'),
+(65, 'Van'),
+(66, 'Yozgat'),
+(67, 'Zonguldak'),
+(68, 'Aksaray'),
+(69, 'Bayburt'),
+(70, 'Karaman'),
+(71, 'Kırıkkale'),
+(72, 'Batman'),
+(73, 'Şırnak'),
+(74, 'Bartın'),
+(75, 'Ardahan'),
+(76, 'Iğdır'),
+(77, 'Yalova'),
+(78, 'Karabük'),
+(79, 'Kilis'),
+(80, 'Osmaniye'),
+(81, 'Düzce');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
